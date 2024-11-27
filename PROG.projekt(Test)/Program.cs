a458969023fc19db4f2349675b1605fa4c939a0e
SimonStefanak <146921280+SimonStefanak@users.noreply.github.com>
2024-11-27 17:35:48 +0100

    zatail velmi na hrubo hotove

--- PROG.projekt(Test)/Program.cs
+++ PROG.projekt(Test)/Program.cs
@@ -12,7 +12,9 @@ namespace MyApp
             int cisloriadku = 0;
             string riadok;
             string riadok1 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(6 - 1);
-            
+            string riadok2 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(12 - 1);
+            string riadok3 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(18 - 1);
+
 
 
             while (cisloriadku < 5 && (riadok = subor.ReadLine()) != null)
@@ -38,17 +40,58 @@ namespace MyApp
                         Console.WriteLine(riadok);
                         cisloriadku++;
                     }
+                    break;
                 }
                 else
                     Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
                     odpoved = Console.ReadLine();
             }
             
+            Console.WriteLine("zadaj svoju odpoved:");
+            string odpoved2 = Console.ReadLine();
 
+            while (true)
+            {
+                if ((riadok = subor.ReadLine()) != null)
+                {
+                    cisloriadku++;
+                }
 
+                if (odpoved2 == riadok2)
+                {
+                    while (cisloriadku < 17 && (riadok = subor.ReadLine()) != null)
+                    {
+                        Console.WriteLine(riadok);
+                        cisloriadku++;
+                    }
+                    break;
+                }
+                else
+                    Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
+                odpoved = Console.ReadLine();
+            }
+
+            Console.WriteLine("zadaj svoju odpoved:");
+            string odpoved3 = Console.ReadLine();
+
+            while (true)
+            {
+                if ((riadok = subor.ReadLine()) != null)
+                {
+                    cisloriadku++;
+                }
+
+                if (odpoved3 == riadok3)
+                {
+                    Console.WriteLine("uspesne si zvladol test. huraaaaaaaa");
+                    break;
+                }
+                else
+                    Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
+                odpoved = Console.ReadLine();
+            }
 
             subor.Close();
-
         }
     }
 }
