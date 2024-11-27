113dc7c5716f07110e32c43459e2fbc4aa7a504a
SimonStefanak <146921280+SimonStefanak@users.noreply.github.com>
2024-11-27 23:30:27 +0100

    v predoslom kode boli celkom dolezite chyby ktore su teraz opravene (zle sa vypisovali riadky zo suboru)

--- PROG.projekt(Test)/Program.cs
+++ PROG.projekt(Test)/Program.cs
@@ -10,11 +10,14 @@ namespace MyApp
         {
             StreamReader subor = new StreamReader("C:\\Users\\kille\\Desktop\\SW-test.txt");
             int cisloriadku = 0;
+            int PocetNespravnychOdpovedi = 0;
             string riadok;
             string riadok1 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(6 - 1);
             string riadok2 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(12 - 1);
             string riadok3 = File.ReadLines("C:\\Users\\kille\\Desktop\\SW-test.txt").ElementAt(18 - 1);
 
+            Console.WriteLine("VITAJ V TOMTO STAR WARS KVIZE. Pre zaciatok stlac ENTER.");
+            Console.ReadKey();
 
 
             while (cisloriadku < 5 && (riadok = subor.ReadLine()) != null)
@@ -28,13 +31,12 @@ namespace MyApp
             
             while(true)
             {
-                if ((riadok = subor.ReadLine()) != null)
-                {
-                    cisloriadku++;
-                }
-
                 if (odpoved == riadok1)
                 {
+                    if ((riadok = subor.ReadLine()) != null)
+                    {
+                        cisloriadku++;
+                    }
                     while (cisloriadku < 11 && (riadok = subor.ReadLine()) != null)
                     {
                         Console.WriteLine(riadok);
@@ -43,8 +45,11 @@ namespace MyApp
                     break;
                 }
                 else
+                {
+                    PocetNespravnychOdpovedi++;
                     Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
                     odpoved = Console.ReadLine();
+                }
             }
             
             Console.WriteLine("zadaj svoju odpoved:");
@@ -52,13 +57,12 @@ namespace MyApp
 
             while (true)
             {
-                if ((riadok = subor.ReadLine()) != null)
-                {
-                    cisloriadku++;
-                }
-
                 if (odpoved2 == riadok2)
                 {
+                    if ((riadok = subor.ReadLine()) != null)
+                    {
+                        cisloriadku++;
+                    }
                     while (cisloriadku < 17 && (riadok = subor.ReadLine()) != null)
                     {
                         Console.WriteLine(riadok);
@@ -67,8 +71,11 @@ namespace MyApp
                     break;
                 }
                 else
+                {
+                    PocetNespravnychOdpovedi++;
                     Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
-                odpoved = Console.ReadLine();
+                    odpoved2 = Console.ReadLine();
+                }
             }
 
             Console.WriteLine("zadaj svoju odpoved:");
@@ -76,22 +83,23 @@ namespace MyApp
 
             while (true)
             {
-                if ((riadok = subor.ReadLine()) != null)
-                {
-                    cisloriadku++;
-                }
-
                 if (odpoved3 == riadok3)
                 {
+                    if ((riadok = subor.ReadLine()) != null)
+                    {
+                        cisloriadku++;
+                    }
                     Console.WriteLine("uspesne si zvladol test. huraaaaaaaa");
+                    Console.WriteLine("Mal si " + PocetNespravnychOdpovedi + " nespravnych odpovedi");
                     break;
                 }
                 else
+                {
+                    PocetNespravnychOdpovedi++;
                     Console.WriteLine("nespravna odpoved, zadaj odpoved znova ");
-                odpoved = Console.ReadLine();
+                    odpoved3 = Console.ReadLine();
+                }
             }
-
-            subor.Close();
         }
     }
 }
